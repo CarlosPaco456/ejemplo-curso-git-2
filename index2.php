675069d4df4e79c2c3b54d7bbe72de26efe979db
carlos <cpacoquispe@gmail.com>
2025-07-21 20:10:20 -0400

    # This is a combination of 3 commits. # This is the 1st commit message:
    archivo1
    
    # This is the commit message #2:
    
    archivo2 modificado
    
    # This is the commit message #3:
    
    archivo3

---
+++ index2.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+$_SESSION['nombre'] = 'Juan';
+var_dump($_SESSION);
+// $uno = 'uno';
+$uno = 1;
+$dos = 'dos';
+$tres = $dos;
+echo $$tres;
+// echo $uno;
+?>
+<h1>hola</h1>
+<h1>hola2</h1>
